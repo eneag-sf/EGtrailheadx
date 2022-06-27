1def7fa60ccb2900c4eaf919c6caf290a132117c
eneag-sf <enea.gjoka4@gmail.com>
2022-06-27 13:35:32 +0200

    changes

--- scripts/fetchScratchOrg.sh
+++ scripts/fetchScratchOrg.sh
@@ -6,13 +6,16 @@ if [ -z $1 ]
 fi
 
 # Fetch Scratch Org from Pool
-sfpowerkit:pool:fetch -t dev 
+sfdx sfpowerkit:pool:fetch -t dev
+
+# Set org alias to make it easier to distinguish scratch orgs
+sfdx force:alias:set YourAlias=$1
 
 #Set as defult org
 sfdx config:set defaultusername=$1
 
 # Assign standard Sales User permission set to user
-sfdx force:user:permset:assign -u $1 -n SalesUserPsl
+#sfdx force:user:permset:assign -u $1 -n SalesUserPsl
 
 #push metadata
 sfdx force:source:push -f -u $1 && echo "Succesfully pushed the metadata to the empty scratch org 😎" || {
@@ -24,8 +27,6 @@ sfdx force:source:push -f -u $1 && echo "Succesfully pushed the metadata to the 
 #sfdx force:data:tree:import -u $1 -p ./data/plan.json --loglevel DEBUG && echo "Added some data to play around with 🐎" ||
     #echo "Not possible to insert the data records. However, you still have some standard data records in your scratch org."
 
-# remove deleted or expired scratch orgs
-sfdx force:org:list --clean --noprompt
 
 # Reset tracking to avoid unnessesary changes like layouts being pulled
 sfdx force:source:tracking:reset -u $1 --noprompt
