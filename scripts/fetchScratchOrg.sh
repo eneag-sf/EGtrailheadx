eec6287b6da53a8391030b5dd366aea4516e7c24
eneag-sf <enea.gjoka4@gmail.com>
2022-07-05 11:21:00 +0200

    commented out some parts

--- scripts/fetchScratchOrg.sh
+++ scripts/fetchScratchOrg.sh
@@ -20,15 +20,15 @@ sfdx config:set defaultusername=$1
 
 # Push latest changed metadata to Scratch Org
 # Don't forget that the scratch Orgs are created at 5AM and with the latest packages published. Developers might have already made changes to the metadata/records so this steps makes sure that the latest metadata is avaliable
-sfdx force:source:push -f -u $1
+#sfdx force:source:push -f -u $1
 
 # Get input if data should be re-migrated to the fetched org
-read -p "Do you want to re-migrate data (y/n)?"
-if [ "$REPLY" = "y" ]; then
-   sfdx sfdmu:run -s csvfile -u $1 -c -n -p data
-  if [ "$REPLY" = "yes" ]; then
-   sfdx sfdmu:run -s csvfile -u $1 -c -n -p data
-fi
+#read -p "Do you want to re-migrate data (y/n)?"
+#if [ "$REPLY" = "y" ]; then
+#   sfdx sfdmu:run -s csvfile -u $1 -c -n -p data
+#  if [ "$REPLY" = "yes" ]; then
+#   sfdx sfdmu:run -s csvfile -u $1 -c -n -p data
+#fi
 
 # Reset tracking to avoid unnessesary changes like layouts being pulled
 sfdx force:source:tracking:reset -u $1 --noprompt
